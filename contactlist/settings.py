f2412810a1524abcd77e06bda9579c932649e4c0
Shokhjakhon Komiljonov <komiljonovshohjahon@gmail.com>
2021-02-03 18:41:04 +0900

    edit

--- contactlist/settings.py
+++ contactlist/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '14h$e+khy=+fshwoyj+zo)ec0fqbu(nxscxcscd7fh@_lv!26x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
