f87481678f61d209fec06a0ace3952ac9cd4b608
Shokhjakhon Komiljonov <komiljonovshohjahon@gmail.com>
2021-02-21 03:55:43 +0900

    Some updates

--- contactlist/settings.py
+++ contactlist/settings.py
@@ -22,9 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '14h$e+khy=+fshwoyj+zo)ec0fqbu(nxscxcscd7fh@_lv!26x'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
